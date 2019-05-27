d93269d1d0fd15648f4c07c240f6a1597a877435
Jorgo Papa <jp.jorgopapa@gmail.com>
2019-05-27 20:07:27 +0200

    Implement Components inside App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
+// components
+import SearchPanel from './SearchPanel'
+import SearchResults from './SearchResults'
+// assets
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    data: [],
+    searchTerm: ''
+  }
+  componentDidMount() {
+    fetch(`https://data.nasa.gov/resource/gh4g-9sfh.json?$limit=40&$q=${this.state.searchTerm}`)
+      .then(res => {
+        return res.json()
+      })
+      .then(data => {
+        console.log(data)
+        this.setState({data})
+      })
+  }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('this is ' + e.target.value);
+    fetch(`https://data.nasa.gov/resource/gh4g-9sfh.json?$limit=10&$q=${this.state.searchTerm}`)
+    .then(res => {
+      return res.json()
+    })
+    .then(data => {
+      console.log(data)
+      this.setState({data})
+    })
+  }
+  handleChange = (e) => {
+    this.setState({searchTerm: e.target.value})
+  }
+  render() {
+      return (
+        <div className="App">
+          <header className="App-header">
+            Meteorite-Explorer
+          </header>
+          {/* Search Panel */}
+          <SearchPanel handleSubmit={this.handleSubmit}
+                        handleChange={this.handleChange}
+                        searchTerm={this.state.searchTerm} />
+          {/* Search Results */}
+          <SearchResults headers={["Name", "ID", "Name Type", "Rec Class", "Mass (g)", "Fall", "Year", "Latitude", "Longitude"]}
+                          values={this.state.data}/>
+        </div>
+      );
+  }
 }
 
 export default App;
